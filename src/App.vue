3ad2f64bbb9e1d635a1a07df22666affc829b5c1
mrchazaaa <charlie.howlett@live.co.uk>
2019-10-10 21:54:57 +0100

    basic form setup

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,22 @@
 <template>
   <div id="app">
-    <form>
-      <fieldset>
-        <h3>Yo checkout my form</h3>
-        <div class="form-group">
-          <label for="exampleSelect1">Example select</label>
-          <select class="form-control" id="exampleSelect1">
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </select>
-        </div>
-        <div class="form-group">
-          <label for="exampleTextarea">Example textarea</label>
-          <textarea class="form-control" id="exampleTextarea" rows="1"></textarea>
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
-      </fieldset>
+    <form @submit.prevent="submitted" id="form-element">
+      <h3>Yo checkout my form</h3>
+      <div class="form-group">
+        <label for="exampleSelect1">Example select</label>
+        <select class="form-control" id="exampleSelect1" v-model="selection">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="exampleTextarea">Example textarea</label>
+        <textarea class="form-control" id="exampleTextarea" rows="1" v-model="name"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
 
     <!-- <ResultsTable/> -->
@@ -26,14 +24,30 @@
 </template>
 
 <script>
-import ResultsTable from './components/ResultsTable.vue'
+import ResultsTable from "./components/ResultsTable.vue";
+import {CLIENT_ID, API_KEY} from "./google-api.js";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
     ResultsTable
+  },
+  formElement: "#form-element",
+  data() {
+    return {
+      selection: "3",
+      name: "mr jenkins"
+    };
+  },
+  methods: {
+    submitted() {
+
+      console.log(CLIENT_ID);
+      console.log(API_KEY);
+
+    }
   }
-}
+};
 </script>
 
 <style>

1b1f14bb38517038b74b456337c085e8769b9186
mrchazaaa <charlie.howlett@live.co.uk>
2019-10-11 12:55:01 +0100

    put main content into form component and established connection with google api cdn

--- src/App.vue
+++ src/App.vue
@@ -1,55 +1,22 @@
 <template>
-  <div id="app">
-    <form @submit.prevent="submitted" id="form-element">
-      <h3>Yo checkout my form</h3>
-      <div class="form-group">
-        <label for="exampleSelect1">Example select</label>
-        <select class="form-control" id="exampleSelect1" v-model="selection">
-          <option>1</option>
-          <option>2</option>
-          <option>3</option>
-          <option>4</option>
-          <option>5</option>
-        </select>
-      </div>
-      <div class="form-group">
-        <label for="exampleTextarea">Example textarea</label>
-        <textarea class="form-control" id="exampleTextarea" rows="1" v-model="name"></textarea>
-      </div>
-      <button type="submit" class="btn btn-primary">Submit</button>
-    </form>
-
-    <!-- <ResultsTable :numOfRows="6"/> -->
-    <ResultsTable v-bind:numOfRows="this.selection"/>
+  <div id="app" class = "text-center">
+    <button class="btn btn-primary">Signin With Google</button>
+    <Form class="d-none"/>
   </div>
 </template>
 
 <script>
-import ResultsTable from "./components/ResultsTable.vue";
-import {CLIENT_ID, API_KEY} from "./google-api.js";
+import Form from "./components/Form.vue";
+import { handleClientLoad } from "./google-api.js";
 
 export default {
   name: "app",
   components: {
-    ResultsTable
+    Form
   },
-  formElement: "#form-element",
-  data() {
-    return {
-      selection: "3",
-      name: "mr jenkins"
-    };
+  mounted: function() {
+    handleClientLoad();
   },
-  methods: {
-    submitted() {
-      
-      console.log(this.selection);
-      console.log(typeof(this.selection));
-      console.log(CLIENT_ID);
-      console.log(API_KEY);
-
-    }
-  }
 };
 </script>
 
---
+++ src/components/Form.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <form @submit.prevent="submitted" id="form-element">
+      <h3>Yo checkout my form</h3>
+      <div class="form-group">
+        <label for="exampleSelect1">Example select</label>
+        <select class="form-control" id="exampleSelect1" v-model="selection">
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </select>
+      </div>
+      <div class="form-group">
+        <label for="exampleTextarea">Example textarea</label>
+        <textarea class="form-control" id="exampleTextarea" rows="1" v-model="name"></textarea>
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+
+    <ResultsTable v-bind:numOfRows="this.selection" />
+  </div>
+</template>
+
+<script>
+import ResultsTable from "./ResultsTable.vue";
+
+export default {
+  name: "Form",
+  components: {
+    ResultsTable
+  },
+  formElement: "#form-element",
+  data() {
+    return {
+      selection: "3",
+      name: "mr jenkins"
+    };
+  },
+  methods: {
+    submitted() {
+      
+      console.log(this.selection);
+      console.log(typeof(this.selection));
+      console.log(CLIENT_ID);
+      console.log(API_KEY);
+
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.table {
+  margin-top: 30px;
+  border: solid black 2px;
+}
+thead {
+  border-top: none;
+  color: white;
+}
+</style>
